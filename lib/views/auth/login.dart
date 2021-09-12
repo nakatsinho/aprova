422d2acada3163c30cffe850311f5373c1cbe34a
nakatsinho <nakatsinho@gmail.com>
2021-09-12 17:02:39 +0200

    Beginning Fields Validation

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -25,65 +25,93 @@ class _LoginState extends State<Login> {
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Image.asset("assets/background.jpg", fit: BoxFit.fill),
+          Image.asset("assets/background.jpg", fit: BoxFit.cover),
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset("assets/logo.png"),
-                        Text(
-                          "Bem vindo!",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 45.0),
-                        ),
-                        Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-                        Padding(
-                            padding: EdgeInsets.all(20.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                TextFormField(
-                                  controller: nameController,
-                                  style: TextStyle(color: Colors.white),
-                                  cursorColor: Colors.white,
-                                  decoration: InputDecoration(
-                                    labelText: "Nome do Usuario".padLeft(20),
-                                    border: UnderlineInputBorder(),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Center(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset(
+                            "assets/logo.png",
+                            width: 250,
+                          ),
+                          Text(
+                            "Bem vindo!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 45.0),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Column(
+                                children: <Widget>[
+                                  TextFormField(
+                                    controller: nameController,
+                                    style: TextStyle(color: Colors.white),
+                                    cursorColor: Colors.white,
+                                    decoration: InputDecoration(
+                                        labelText:
+                                            "Nome do Usuario".padLeft(20),
+                                        labelStyle: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: "Lato-Light",
+                                            letterSpacing: 1.5),
+                                        enabledBorder: UnderlineInputBorder(
+                                          borderSide:
+                                              BorderSide(color: Colors.white),
+                                        ),
+                                        focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.white))),
                                   ),
-                                ),
-                                TextFormField(
-                                  controller: passwordController,
-                                  style: TextStyle(color: Colors.white),
-                                  cursorColor: Colors.white,
-                                  decoration: InputDecoration(
-                                      labelText: "Senha".padLeft(10),
-                                      border: UnderlineInputBorder()),
-                                ),
-                                DefaultButton(
-                                  text: "Iniciar Sessao".toUpperCase(),
-                                  press: () {},
-                                ),
-                                Text(
-                                  "Nao tem conta?",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w200),
-                                )
-                              ],
-                            ))
-                      ],
+                                  Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10)),
+                                  TextFormField(
+                                    controller: passwordController,
+                                    style: TextStyle(color: Colors.white),
+                                    cursorColor: Colors.white,
+                                    decoration: InputDecoration(
+                                        labelText: "Senha".padLeft(10),
+                                        labelStyle: TextStyle(
+                                            color: Colors.white,
+                                            fontFamily: "Lato-Light",
+                                            letterSpacing: 1.5),
+                                        enabledBorder: UnderlineInputBorder(
+                                          borderSide:
+                                              BorderSide(color: Colors.white),
+                                        ),
+                                        focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.white))),
+                                  ),
+                                  Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 20)),
+                                  DefaultButton(
+                                    text: "Iniciar Sessao".toUpperCase(),
+                                    press: () {},
+                                  ),
+                                ],
+                              )),
+                          Text(
+                            "Nao tem conta ?",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w200),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
